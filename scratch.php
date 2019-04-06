61b76ae8c6218a6e8ce59f058188285687c4d1c9
OZON-SIBERIA <medvedev152@live.com>
2019-04-07 00:00:11 +1000

    piton3

--- scratch.php
+++ scratch.php
@@ -1,33 +1,27 @@
 <html>
-<style>
-    div {
-        background: aqua;
-    }
-</style>
 <center>
     <img src="https://cs8.pikabu.ru/images/big_size_comm/2016-04_4/146080718119713034.jpg" alt="Сорри, питон">
     </br>
-
-
     <?php
-		if((date("5:0:0")) < (date("h:i:s")) and (date("h:i:s")) < (date("12:0:0"))) {echo("Доброе утро, питон");}
-		elseif((date("12:0:0")) < (date("h:i:s")) and (date("h:i:s")) < (date("18:0:0"))) {echo("Добрый день, питон");}
-		elseif((date("18:0:0")) < (date("h:i:s")) and (date("h:i:s")) < (date("23:0:0"))) {echo("Добрый вечер, питон");}
+    $current_timezone = new DateTimeZone("Asia/Vladivostok");
 
-        echo "ЭТО ВАМ НЕ ЭТО<br/>";
-        echo(date(" h:i:s "));
-        echo (date(" d.F. Y "));
+    $a = new DateTime("now", $current_timezone);
+    $morning = new DateTime("5:00:00", $current_timezone);
+    $dinner = new DateTime("12:00:00", $current_timezone);
+    $evening = new DateTime("18:00:00", $current_timezone);
+    $night = new DateTime("23:59:59", $current_timezone);
+    if($morning<=$a && $a<=$dinner) {
+        echo("доброе утро, питон");
+    } elseif ($dinner<$a && $a<=$evening){
+        echo("добрый день, питон");
+    } elseif($evening<$a && $a<=$night) {
+        echo ("добрый вечер, питон");
+    } else {
+        echo("доброй ночи, питон");
+    }
     ?>
 </center>
 </html>
 
 
-dadwad
 
-<?php
-    $a = new DateTime();
-    echo($a->format('c'));
-
-
-
-?>
