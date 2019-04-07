7dfcc2d1b00fe332c7d43674a43e4c4a4f7d2483
OZON-SIBERIA <medvedev152@live.com>
2019-04-07 22:50:38 +1000

    Update todolist_entry_page.php

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -41,31 +41,7 @@
     <button type="submit"  name="submit" id="add_button" class="add_button">Add Task</button>
 </form>
 
-<table>
-    <thead>
-    <tr>
-        <th>N</th>
-        <th>tasks</th>
-        <th style-"width: 60px;">Action</th>
-    </tr>
-    </thead>
 
-    <tbody>
-    <?php
-    $tasks = mysqli_query($db, "SELECT*FROM tasks");
-
-    $i = 1; while ($row = mysqli_fetch_array($tasks)){
-        ?>
-    <tr>
-    <td> <?php echo $i; ?> </td>
-    <td class="task"> <?php echo $row['task']; ?> </td>
-    <td class="delete">
-        <a href="todolist_entry_page.php ?> del_task=<?php echo $row['id'] ?>">x</a>
-    </td>
-    </tr>
-    <?php $i++; } ?>
-    </tbody>
-</table>
 </body>
 </html>
 
