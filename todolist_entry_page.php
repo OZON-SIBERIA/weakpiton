e99a29699a180c50f52584075583fc3187dfd33c
OZON-SIBERIA <medvedev152@live.com>
2019-04-08 23:47:29 +1000

    without_back_todolist_v0.6.2

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -1,22 +1,5 @@
-    <?php/*
-    $connection = mysqli_connect('localhost', 'root', '122435606', 'todolist_database', '3306', '1050');
-    $query_get = 'SELECT * FROM tasks';
-    $errors = "";
-    if(isset($_POST['submit'])) {
-        if(empty($_POST['task'])) {
-            $errors = "You must fill in the task";
-        }
-        else {
-            $task = $_POST['task'];
-            $query_insertion ="INSERT INTO tasks (task) VALUES ($task)";
-            $insertion = mysqli_query($connection, $query_insertion);
-            header('location: todolist_entry_page.php');
-        }
-    }
-    */?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <title>Old Todolist</title>
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -26,13 +9,6 @@
     <h2> Old Todolist for old pitons</h2>
 </div>
 <form method="post" action="todolist_entry_page.php" class="input_form">
-    <?php
-        if (isset($errors)) {
-            ?>
-    <p> <?php echo $errors; ?> </p>p>
-    <?php
-        }
-    ?>
     <input type="text" name="task" class="task_input">
     <button type="submit"  name="submit" id="add_button" class="add_button">Add Task</button>
 </form>
@@ -45,22 +21,12 @@
     </tr>
     </thead>
     <tbody>
-    <?php
-        /*$tasks = mysqli_query($connection, $query_get);*/
-        $i =1;
-        /*while($row = mysqli_fetch_array($tasks)) {*/
-            ?>
     <tr>
-        <td> <?php echo $i; ?> </td>
-        <td class = "task"> <?php/* echo $row['task'];*/ ?> </td>
+        <td></td>
+        <td class = "task"></td>
         <td class= "delete">
-
         </td>
     </tr>
-    <?php
-    $i++;
-        }
-    ?>
     </tbody>
 </table>
 </body>
