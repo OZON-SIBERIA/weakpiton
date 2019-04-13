9a03877722407122f8696c535d3b9a10f8a5ba0a
OZON-SIBERIA <medvedev152@live.com>
2019-04-13 21:27:15 +1000

    Update todolist_entry_page.php

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once "db_settings.php";
     try {
         $DBH = new PDO("mysql:$host;dbname=$dbname", $user, $pass);
