1d2baa5f25eae6847e71373df3eee5056671cb70
OZON-SIBERIA <medvedev152@live.com>
2019-04-13 21:33:26 +1000

    Update todolist_entry_page.php

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 require_once "db_settings.php";
     try {
         $DBH = new PDO("mysql:$host;dbname=$dbname", $user, $pass);
+        $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $msg) {
         echo $msg->getMessage();
