1e1b047d04937c0536c13e24ac4dedba9df701c2
OZON-SIBERIA <medvedev152@live.com>
2019-04-18 00:10:57 +1000

    tdl_upd

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -12,12 +12,18 @@ require_once "db_settings.php";
     }
     $insertion = $DBH->prepare("INSERT INTO todolist_database.tasks (task) VALUES (:task)");
     $selection = $DBH->prepare("SELECT * FROM todolist_database.tasks");
-    /*$deletion = $DBH->prepare("DELETE FROM todolist_database.tasks WHERE ");*/
+    $deletion = $DBH->prepare("DELETE FROM todolist_database.tasks WHERE 'id'=:id LIMIT 1");
     if (!empty($_POST["task"])) {
         $task = $_POST["task"];
         $insertion->bindParam(':task', $task);
         $insertion->execute();
         header("Location: /todolist_entry_page.php");
+        exit;
+    }
+    if (!empty($_GET['delete_task'])) {
+        $id = $_GET['delete_task'];
+        $deletion->bindParam(':id', $id);
+        $deletion->execute();
         exit;
     }
 ?>
@@ -61,7 +67,7 @@ require_once "db_settings.php";
             foreach ($rows as $row) { ?>
     <tr>    <td class="id"><?php echo $i; $i++; ?> </td>
             <td class="selection"> <?php echo $row['task']; ?> </td>
-            <td class="delete"> <a>x</a> </td>
+            <td class="delete"> <a href="todolist_entry_page.php?delete_task=<?php echo $row['id'] ?>">x</a> </td>
                 <?php echo "<br/>"; ?>
     </tr> <?php } ?>
     </tbody>
