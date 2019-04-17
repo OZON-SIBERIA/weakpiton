30ecd79d3aabfc11d4b213a98e88e8b2547a804d
OZON-SIBERIA <medvedev152@live.com>
2019-04-17 22:32:10 +1000

    tdl_upd

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -12,7 +12,7 @@ require_once "db_settings.php";
     }
     $insertion = $DBH->prepare("INSERT INTO todolist_database.tasks (task) VALUES (:task)");
     $selection = $DBH->prepare("SELECT task FROM todolist_database.tasks");
-    $deletion = $DBH->prepare("DELETE")
+    /*$deletion = $DBH->prepare("DELETE");*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,13 +32,14 @@ require_once "db_settings.php";
     <input type="text" name="task" class="task_input">
     <button type="submit"  name="submit" id="add_button" class="add_button">Add Task</button>
     <H><?php
-    if (!empty($_POST['submit'])) {
-        if (empty($_POST['task'])) {
+        $task = $_POST['task'];
+        $submit = $_POST['submit'];
+    if (!empty($submit)) {
+        if (empty($task)) {
             $input_error = "Would you kindly, enter the task";
             echo $input_error;
         }
         else {
-            $task = $_POST['task'];
             $insertion->bindParam(':task', $task);
             $insertion->execute();
         }
