0f15b61a209e62df30717f7edf16b6b615293e49
OZON-SIBERIA <medvedev152@live.com>
2019-04-20 17:30:23 +1000

    cl_upd

--- weak_clock.php
+++ weak_clock.php
@@ -16,9 +16,9 @@ if (!empty($_POST("time_set")) & !empty($_POST("score_set"))) {
 <script type="text/javascript" src="jquery-3.4.0.js">
 </script>
     <form method="post" action="weak_clock.php" class="input_form">
-        <input type="datetime-local" name="time_set" class="time_input">
+        <input type="time" name="time_set" class="time_input">
         <button type="submit"  name="submit_time" id="set_time_button">Set Time</button>
-        <input type="datetime-local" name="score_set" class="score_input">
+        <input type="time" name="score_set" class="score_input">
         <button type="submit"  name="submit_score" id="set_score_button" >Set Score</button>
     </form>
 </body>
