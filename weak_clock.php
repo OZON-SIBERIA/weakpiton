fac3d7b92d6248bbde750d2378478a2f5a1835bd
OZON-SIBERIA <medvedev152@live.com>
2019-04-20 17:37:52 +1000

    cl_upd

--- weak_clock.php
+++ weak_clock.php
@@ -10,14 +10,13 @@
     <form method="post" action="weak_clock.php" class="input_form">
         <input type="time" name="time_set" class="time_input">
         <button type="submit"  name="submit_time" id="set_time_button">Set Time</button>
-        <input type="time" name="score_set" class="score_input">
+        <input type="input" name="score_set" class="score_input">
         <button type="submit"  name="submit_score" id="set_score_button" >Set Score</button>
     </form>
 </body>
 <?php/*
 if (!empty($_POST("time_set")) & !empty($_POST("score_set"))) {
     $time = new DateTime();
-    $score = new DateTime();
     $time = $_POST("time_set");
     $score = $_POST("score_set");
     echo $time;
