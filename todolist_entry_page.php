d860cbf92696156563f4282161fc0e80e8e8b5b5
OZON-SIBERIA <medvedev152@live.com>
2019-04-20 14:19:48 +1000

    tdl_upd

--- todolist_entry_page.php
+++ todolist_entry_page.php
@@ -2,7 +2,7 @@
 require_once "db_settings.php";
     try {
         $DBH = new PDO("mysql:$host;dbname=todolist_database", $user, $pass);
-        $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES, false);
     }
     catch (PDOException $msg) {
         echo $msg->getMessage();
